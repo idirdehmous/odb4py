a420ab47cd14b1c8067d0d761dd532ff848cb86a
idir Dehmous <idirdehmous@gmail.com>
2026-03-30 22:21:45 +0200

    Add map_type function , map ODB and sqlite tables datatypes

--- include/pyspam.h
+++ include/pyspam.h
@@ -3,7 +3,7 @@
 
 
 // Version
-#define ODB4PY_VERSION "1.3.1"
+#define ODB4PY_VERSION "1.3.2rc1"
 
 
 
@@ -18,28 +18,21 @@
 #include  "odbdump.h"
 #define SMAX 50 
 
-//  The unclude file could be renamed diffently , BUT  it stays  "pyspam.h"
-//  since with a module called  pyspam.c  that i learned writing 
-//  C extension for py  :-)  ... always remember !
-//  Link could be found here :
-//  https://docs.python.org/3/extending/extending.html  
-//  Valid the : 11/03/2026
 
-
- /* For description :
- * In this package these lines have been taken from  :   include/privpub.h  
+/*
+ * These lines have been taken from  :   include/privpub.h  
  * 
-#define DATATYPE_UNDEF                0x0           (0 dec) undef 
-#define DATATYPE_BIT                  0x100         (256 dec) bit : 1 bits, 0 bytes 
-#define DATATYPE_INT1                 0x10f         (271 dec) char : 8 bits, 1 bytes 
+#define DATATYPE_UNDEF                0x0           (0 dec)   undef 
+#define DATATYPE_BIT                  0x100         (256 dec) bit   : 1 bits, 0 bytes 
+#define DATATYPE_INT1                 0x10f         (271 dec) char  : 8 bits, 1 bytes 
 #define DATATYPE_INT2                 0x113         (275 dec) short : 16 bits, 2 bytes 
-#define DATATYPE_INT4                 0x117         (279 dec) int : 32 bits, 4 bytes 
+#define DATATYPE_INT4                 0x117         (279 dec) int   : 32 bits, 4 bytes 
 #define DATATYPE_INT8                 0x11b         (283 dec) long long : 64 bits, 8 bytes 
 #define DATATYPE_UINT1                0x10e         (270 dec) uchar : 8 bits, 1 bytes 
 #define DATATYPE_UINT2                0x112         (274 dec) ushort : 16 bits, 2 bytes 
-#define DATATYPE_UINT4                0x116         (278 dec) uint : 32 bits, 4 bytes 
+#define DATATYPE_UINT4                0x116         (278 dec) uint   : 32 bits, 4 bytes 
 #define DATATYPE_UINT8                0x11a         (282 dec) ulonglong : 64 bits, 8 bytes 
-#define DATATYPE_REAL4                0x216         (534 dec) float : 32 bits, 4 bytes 
+#define DATATYPE_REAL4                0x216         (534 dec) float  : 32 bits, 4 bytes 
 #define DATATYPE_REAL8                0x21a         (538 dec) double : 64 bits, 8 bytes 
 #define DATATYPE_REAL16               0x21e         (542 dec) long double : 128 bits, 16 bytes 
 #define DATATYPE_CMPLX4               0x31a         (794 dec) complex4 : 64 bits, 8 bytes 
@@ -59,44 +52,40 @@
 #define DATATYPE_VARCHAR              0x20002c      (2097196 dec) varchar(1:255) : 256 bytes 
 */
 
-
-
-
-//  Map ODB1 and sqlite datatypes 
+// Map ODB datatype to SQLITE ones 
 const char* map_type(int odb_type) {
     switch (odb_type) {
-        case DATATYPE_UNDEF:
-	     return NULL ; 
-	case DATATYPE_INT1:
-	case DATATYPE_INT2:
-	case DATATYPE_INT4:
-	case DATATYPE_INT8:
-	case DATATYPE_UINT1:
-	case DATATYPE_UINT2:
-	case DATATYPE_UINT4:
-	case DATATYPE_UINT8:
-            return "INTEGER";
+	    case DATATYPE_BITFIELD:
+	    case DATATYPE_INT1:
+	    case DATATYPE_INT2:
+	    case DATATYPE_INT4:
+	    case DATATYPE_INT8:
+	    case DATATYPE_UINT1:
+	    case DATATYPE_UINT2:
+	    case DATATYPE_UINT4:
+	    case DATATYPE_UINT8:
+            case DATATYPE_YYYYMMDD:
+            case DATATYPE_HHMMSS:
+            case DATATYPE_LINKOFFSET:
+	    case DATATYPE_LINKLEN:
+	    case DATATYPE_BUFR:
+            case DATATYPE_GRIB:
+                return "INTEGER";
+
         case DATATYPE_REAL4:
         case DATATYPE_REAL8:
         case DATATYPE_REAL16:
-            return "REAL";
+                return "REAL";
+
         case DATATYPE_STRING:
 	case DATATYPE_CHAR:
-	case DATATYPE_VARCHAR:
-            return "TEXT";
-        case DATATYPE_BITFIELD:
-	case DATATYPE_YYYYMMDD:
-        case DATATYPE_HHMMSS:
-	case DATATYPE_LINKOFFSET:
-	case DATATYPE_LINKLEN:
-	case DATATYPE_BUFR:
-	case DATATYPE_GRIB:
-            return "INTEGER";
+        case DATATYPE_VARCHAR:
+                return "TEXT";
+
         default:
-            return "REAL";  // fallback 
+                return "REAL";  // fallback 
     }
 }
-
 
 
 /*    Destroy np Capsule   */
