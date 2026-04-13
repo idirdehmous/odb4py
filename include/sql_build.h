80928455326e777d2ad99c0ffa8a5adfcfdc826b
idir Dehmous <idirdehmous@gmail.com>
2026-04-14 01:09:30 +0200

    Clean and comment

--- include/sql_build.h
+++ include/sql_build.h
@@ -2,7 +2,6 @@
 #define SQL_BUILDER_H
 
 // Concat sql parts for geo_module.c  
-
 typedef struct {
     PyObject *parts;   // python SQL parts 
 } SQLBuilder;
@@ -12,7 +11,6 @@ static int
 is_blank_string(const char *s)
 {
     if (!s) return 1;
-
     while (*s) {
         if (!isspace((unsigned char)*s))
             return 0;   //  find a character 
@@ -20,6 +18,7 @@ is_blank_string(const char *s)
     }
     return 1;           // spaces only 
 }
+
 
 static SQLBuilder * sqlbuilder_new(void)
 {
@@ -47,24 +46,19 @@ sqlbuilder_addf(SQLBuilder *b, const char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
-
     va_list args_copy;
     va_copy(args_copy, args);
-
     int needed = vsnprintf(NULL, 0, fmt, args_copy);
     va_end(args_copy);
-
     if (needed < 0) {
         va_end(args);
         return -1;
     }
-
     char *buffer = PyMem_Malloc(needed + 1);
     if (!buffer) {
         va_end(args);
         return -1;
     }
-
     vsnprintf(buffer, needed + 1, fmt, args);
     va_end(args);
     PyObject *s = PyUnicode_FromString(buffer);
@@ -76,9 +70,7 @@ sqlbuilder_addf(SQLBuilder *b, const char *fmt, ...)
     return rc;
 }
 
-
-static PyObject *sqlbuilder_build(SQLBuilder *b)
-{
+static PyObject *sqlbuilder_build(SQLBuilder *b) {
     PyObject *empty = PyUnicode_FromString("");
     if (!empty) return NULL;
     PyObject *sql = PyUnicode_Join(empty, b->parts);
@@ -92,6 +84,4 @@ static void sqlbuilder_free(SQLBuilder *b){
     Py_XDECREF(b->parts);
     PyMem_Free(b);
 }
-
-
 #endif
